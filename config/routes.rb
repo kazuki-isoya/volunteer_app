8231b740ab25d9ffa61895bf469b72a75a0a3c09
moru more <moremoru.world21@gmail.com>
2020-03-30 16:40:25 +0900

    メールの設定。letter_openerの実装。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'volunteers/new'
   get 'volunteers/edit'
   get 'volunteers/show'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
