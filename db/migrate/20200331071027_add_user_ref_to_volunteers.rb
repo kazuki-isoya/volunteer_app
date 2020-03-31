869d7134eb1b8fc0232f6354eab5ac76fe8163ee
moru more <moremoru.world21@gmail.com>
2020-03-31 16:12:02 +0900

    userとvolunteerでアソシエーションを設定

---
+++ db/migrate/20200331071027_add_user_ref_to_volunteers.rb
@@ -0,0 +1,5 @@
+class AddUserRefToVolunteers < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :volunteers, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_044659) do
+ActiveRecord::Schema.define(version: 2020_03_31_071027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,9 @@ ActiveRecord::Schema.define(version: 2020_03_31_044659) do
     t.string "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_volunteers_on_user_id"
   end
 
+  add_foreign_key "volunteers", "users"
 end
