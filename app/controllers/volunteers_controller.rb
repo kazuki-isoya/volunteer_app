bb4224713fe22cff394ee46cf793e811bd7f75d2
moru more <moremoru.world21@gmail.com>
2020-03-31 15:08:21 +0900

    volunteerのコントローラーを調整。デバッグ用のgemを追加。

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,14 +1,46 @@
 class VolunteersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_volunteer, only: [:edit, :update, :show, :destroy]
   def index
+    @volunteers = Volunteer.all
   end
 
   def new
+    @volunteer = Volunteer.new
+  end
+
+  def create
+    @volunteer = Volunteer.new(volunteer_params)
+    if @volunteer.save
+      redirect_to volunteers_path, notice: 'ボランティア募集を作成しました！'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @volunteer.update(volunteer_params)
+      redirect_to volunteers_path, notice: 'ボランティア内容を編集しました。'
+    else
+      render :edit
+    end
+  end
+
   def show
   end
+
+  def destroy
+    @volunteer.destroy
+    redirect_to volunteers_path, notice: 'ボランティアを削除しました。'
+  end
+
+  private
+
+  def set_volunteer
+    @volunteer = Volunteer.find(params[:id])
+  end
+
 end
