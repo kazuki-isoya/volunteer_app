25a163f591ab6527a65e5da7e275c1a2e38119fa
moru more <moremoru.world21@gmail.com>
2020-04-05 15:05:02 +0900

    ボランティア登録時にメール送信する箇所の修正。

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,6 +1,6 @@
 class VolunteerMailer < ApplicationMailer
   def volunteer_mail(volunteer)
-    @volunteer = feed
+    @volunteer = volunteer
     mail to: @volunteer.user.email, subject:"依頼投稿の確認メール"
   end
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,7 +2,7 @@ class Volunteer < ApplicationRecord
   belongs_to :user
   has_many :order_managements, dependent: :destroy
   has_many :contractor, through: :order_managements, source: :user, dependent: :destroy
-  has_many :categorizings
+  has_many :categorizings, dependent: :destroy
   has_many :categories, through: :categorizings
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
