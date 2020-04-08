87a4f3c1608d3d6b35118a19fedd7d266ecd1a99
moru more <moremoru.world21@gmail.com>
2020-04-08 22:13:15 +0900

    volunteerの日付にバリデーションを付けた。rails_adminの日本語化。

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -6,4 +6,10 @@ class Volunteer < ApplicationRecord
   has_many :categories, through: :categorizings
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
+
+  validate :date_is_not_past
+
+  def date_is_not_past
+    errors.add(:date, "が適切ではありません") if date.nil? || date < Date.today
+  end
 end
