7aee8d99cb92c34f867a0eb9e3687f8214d433b4
moru more <moremoru.world21@gmail.com>
2020-04-10 00:36:59 +0900

    メールの修正。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://evening-reef-57995.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://damp-beach-53602.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
