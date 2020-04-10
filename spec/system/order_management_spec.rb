242e1dd01294db3e5f8a2ded27b0534e3e510b50
moru more <moremoru.world21@gmail.com>
2020-04-10 23:52:02 +0900

    orderテストに追記。

--- spec/system/order_management_spec.rb
+++ spec/system/order_management_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe 'ボランティア受注機能', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @user2 = FactoryBot.create(:user, name: "user2", email: "user2@email.com", admin: false)
-    @volunteer = FactoryBot.create(:volunteer, user: @user2)
+    @user3 = FactoryBot.create(:user, name: "user3", email: "user3@email.com", admin: false)
+    @volunteer = FactoryBot.create(:volunteer, capacity: 1, user: @user2)
     visit root_path
     click_on 'ログインする'
     fill_in 'user_email', with: 'user@email.com'
@@ -29,6 +30,19 @@ RSpec.describe 'ボランティア受注機能', type: :system do
       visit volunteer_path(@volunteer.id)
       click_on '受注解除する'
       page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content "受注解除しました"
+    end
+    it "定員が埋まっている" do
+      click_on 'ログアウトする'
+      sleep 1
+      click_on 'ログインする'
+      fill_in 'user_email', with: 'user3@email.com'
+      fill_in 'user_password', with: 'password'
+      click_on 'ログイン'
+      visit volunteer_path(@volunteer.id)
+      click_on '受注する'
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content "受注出来ません"
     end
   end
 
