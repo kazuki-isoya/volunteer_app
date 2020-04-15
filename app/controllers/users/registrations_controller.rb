cbd10d6350c2fac632051c5a4e5b074526e02d24
moru more <moremoru.world21@gmail.com>
2020-04-15 13:27:28 +0900

    テストログイン機能の実装。コメントのデザインを変更。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :forbid_test_user, only: [:edit, :update, :destroy]
 
   # GET /resource/sign_up
   # def new
@@ -60,6 +61,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  def forbid_test_user
+    if @user.email == "test@email.com"
+      flash[:alert] == "テストユーザーは編集できません"
+      redirect_to root_path
+    end
   end
 
   # The path used after sign up for inactive accounts.
