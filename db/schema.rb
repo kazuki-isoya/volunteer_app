7cf5fd4611bb903301e9757da7b5146e65abbe94
moru more <moremoru.world21@gmail.com>
2020-04-16 17:54:44 +0900

    詳細画面のデザインの修正。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_132907) do
+ActiveRecord::Schema.define(version: 2020_04_16_061800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_132907) do
     t.string "name", default: "no name", null: false
     t.string "icon", default: "no image"
     t.boolean "admin", default: false, null: false
+    t.text "introduction", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
