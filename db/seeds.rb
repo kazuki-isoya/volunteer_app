47667be4efaf5a352772c90d74455e0eb61303cf
moru more <moremoru.world21@gmail.com>
2020-04-28 22:41:08 +0900

    seedデータに画像を参照。

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 @test = User.create(
   name: "test",
   email: "test@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/hito.jpg"),
   password: "password",
   admin: false,
   introduction: "テストユーザーはプロフィールを編集できません。"
@@ -12,7 +12,7 @@
 @nobita = User.create(
   name: "のび太",
   email: "nobita@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/nobita.png"),
   password: "password",
   admin: false,
   introduction: "特技は昼寝、あやとり、射撃"
@@ -21,34 +21,34 @@
 @cloud = User.create(
   name: "クラウド",
   email: "cloud@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/cloud.jpg"),
   password: "password",
   admin: false,
   introduction: "元ソルジャー1st。"
 )
 
-@shulk = User.create(
-  name: "シュルク",
-  email: "shulk@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+@miyano = User.create(
+  name: "宮野志保",
+  email: "miyano@email.com",
+  icon:File.open("./app/assets/images/miyano.jpg"),
   password: "password",
   admin: false,
-  introduction: "モナドの研究をしています。"
+  introduction: "本当は18歳よ"
 )
 
-@jojo = User.create(
-  name: "ジョジョ",
-  email: "jojo@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+@sato = User.create(
+  name: "佐藤洋子",
+  email: "sato@email.com",
+  icon:File.open("./app/assets/images/hito2.png"),
   password: "password",
   admin: false,
   introduction: "ハッピーうれぴーよろぴくねー"
 )
 
-@zero = User.create(
-  name: "ゼロ",
+@amuro = User.create(
+  name: "安室透",
   email: "zero@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/amuro.jpg"),
   password: "password",
   admin: false,
   introduction: "黒の組織のスパイで公安の刑事でカフェの店員です"
@@ -57,25 +57,25 @@
 @char = User.create(
   name: "シャア",
   email: "char@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/char.jpg"),
   password: "password",
   admin: false,
   introduction: "本当は総帥です。大佐は愛称です。"
 )
 
 @shin = User.create(
-  name: "シン",
+  name: "新庄狂死郎",
   email: "shin@email.com",
   icon:File.open("./app/assets/images/default.png"),
   password: "password",
   admin: false,
-  introduction: "モビルスーツの操縦には自身があります"
+  introduction: "中2です。"
 )
 
 @eripiyo = User.create(
   name: "えりぴよ",
   email: "eripiyo@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+  icon:File.open("./app/assets/images/eripiyo.jpeg"),
   password: "password",
   admin: false,
   introduction: "岡山在住です。ChamJamのファンです。"
@@ -87,16 +87,16 @@
   icon:File.open("./app/assets/images/default.png"),
   password: "password",
   admin: false,
-  introduction: "おれはソロだ"
+  introduction: "言っとくがおれはソロだ"
 )
 
-@van = User.create(
-  name: "ヴァンガロール",
-  email: "van@email.com",
-  icon:File.open("./app/assets/images/default.png"),
+@toho = User.create(
+  name: "東方不敗",
+  email: "toho@email.com",
+  icon:File.open("./app/assets/images/toho.jpg"),
   password: "password",
   admin: false,
-  introduction: "私は大砲よ"
+  introduction: "東方不敗は王者の風"
 )
 
 
@@ -104,12 +104,12 @@ FactoryBot.create(:volunteer, title: "アプリ作成に協力して", describe:
 FactoryBot.create(:volunteer, title: "薬を届けて", describe: "コロナ対策で外に出ないので病院から薬を持ってきてほしいです。", capacity: 1, user: @test)
 FactoryBot.create(:volunteer, title: "宿題を手伝って", describe: "算数の宿題がわからないので助けて", capacity: 1, user: @nobita)
 FactoryBot.create(:volunteer, title: "なんでも屋の手伝い", describe: "一人では手が足りないから頼みたい。詳しくはコメントで。", capacity: 2, user: @cloud)
-FactoryBot.create(:volunteer, title: "研究の手伝い", describe: "研究材料が必要なので機神兵を10体倒してきて欲しい。", user: @shulk)
-FactoryBot.create(:volunteer, title: "柱の男を倒したい", describe: "ワムウ、エシディシ、カーズを倒すため協力してくれ。", user: @jojo)
-FactoryBot.create(:volunteer, title: "口裏を合わせて欲しい", describe: "アポロの梓さんへの言い訳をするための口裏を合わせて協力して欲しい。", capacity: 2, user: @zero)
+FactoryBot.create(:volunteer, title: "研究の手伝い", describe: "新しく作った薬の被検体になって欲しい", user: @miyano)
+FactoryBot.create(:volunteer, title: "パンを買ってきて欲しい", describe: "年をとって車に乗れないので代わりにパンを買ってきて欲しい", user: @sato)
+FactoryBot.create(:volunteer, title: "口裏を合わせて欲しい", describe: "詳しくは口頭で", capacity: 2, user: @amuro)
 FactoryBot.create(:volunteer, title: "アクシズを落とす", describe: "アクシズを地球に落下させるため協力を求む。", capacity: 100, user: @char)
 FactoryBot.create(:volunteer, title: "ライブに一緒に行って欲しい", describe: "ライブに行って一緒に舞菜を応援して欲しい。更にCDも買って欲しい。", user: @eripiyo)
-FactoryBot.create(:volunteer, title: "チャンピオンになるために", describe: "チャンピオンになるために腕に自身がある者を求む。", capacity: 2, user: @van)
+FactoryBot.create(:volunteer, title: "地球保護", describe: "地球環境を守るためデビルガンダムを復活させたい", capacity: 3, user: @toho)
 
 Category.create(name: "誰でも歓迎", icon:File.open("./app/assets/images/welcom.jpg"))
 Category.create(name: "力仕事", icon:File.open("./app/assets/images/power.png"))
